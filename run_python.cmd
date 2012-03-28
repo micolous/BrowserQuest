a02ddf81f08a79edd928a1e6453cfe6840750c10
Michael Farrell <micolous+git@gmail.com>
2012-03-29 00:15:27 +1030

    Get an environment inside mod_pywebsockets where the client UI can load.

---
+++ run_python.cmd
@@ -0,0 +1,1 @@
+@python -m "mod_pywebsocket.standalone" -p 8000 -d . -w server/python/websocket_handler
---
+++ run_python.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python -m 'mod_pywebsocket.standalone' -p 8000 -d . -w server/python/websocket_handler
